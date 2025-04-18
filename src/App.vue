a03c2e3f33bcccad1d671fe7d227d9b303ba733b
Ethan_nguyen <phuthanh0301@gmail.com>
2025-04-18 15:39:39 -0500

    basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    Hello Vue!
+  <div class="app">
+    <Navigation></Navigation>
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup>
+import Navigation from '@/components/Navigation.vue';
 
 </script>
 
 <style lang="scss" scoped>
-
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
